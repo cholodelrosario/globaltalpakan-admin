f038e82c8444a786ceff67a0a97a1d8c1c1613e1
codeavor-styx <superstyx017@gmail.com>
2021-08-08 05:16:56 +0800

    Get Credits/Bet Transactions

--- src/pages/Players.vue
+++ src/pages/Players.vue
@@ -26,6 +26,8 @@ a<template>
                         <q-td key="action" :props="props">
                             <q-btn flat color="primary" @click="editAgent(props.row)" label="Move Player" />
                             <q-btn flat color="primary" @click="viewBetHistory(props.row)" label="View Bet History" />
+                            <q-btn flat color="primary" @click="viewBetTransaction(props.row)" label="Bet Transactions" />
+                            <q-btn flat color="primary" @click="claimCredits(props.row)" label="Get Credits" />
                         </q-td>
                     </q-tr>
                 </template>  
@@ -108,6 +110,92 @@ a<template>
                 </q-card-actions>
             </q-card>
         </q-dialog>
+        <q-dialog v-model="myPlayersBetTransactions" persistent>
+            <q-card style="width: 550px" class="bg-dark text-white">
+                <q-card-section>
+                <h5 class="text-primary text-weight-bolder q-mt-none">BET TRANSACTIONS</h5>
+                    <q-tabs
+                        v-model="transtab"
+                        inline-label
+                        class="bg-secondary text-white shadow-2 col-12"
+                    >
+                        <q-tab class="col-6" name="bets" label="Game Bet History" />
+                        <q-tab class="col-6" name="options" label="Bet Option History" />
+                        
+                    </q-tabs>
+                </q-card-section>
+
+                <q-card-section class="q-pt-none">
+                    <h6 class="text-secondary flex flex-center text-white q-ma-none">{{this.transtab == 'bet' ? 'BET HISTORY' : 'BET OPTIONS HISTORY'}}</h6>
+                    <q-list v-for="(p, index) in this.transGetData" :key="index" bordered separator>
+                        <!-- <br> -->
+                        <q-item>
+                            <q-item-section caption top>
+                                <q-item-label overline class="text-white">P&nbsp;{{p.amount}} &nbsp;&nbsp;&nbsp;<b :class="p.betStatus === 'LOSE' || p.betStatus === 'CANCELLED' ? 'text-red' : 'text-primary'">{{p.betStatus}}</b></q-item-label>
+                                <q-item-label overline class="text-white">{{p.team.team}}</q-item-label>
+                                <q-item-label v-if="tab == 'option'" caption class="text-white">{{p.betOptions.name}}</q-item-label>
+                                <q-item-label caption class="text-white"></q-item-label>
+                                <!-- <q-item-label overline class="text-white">Total Winnings: &nbsp;{{p.betStatus == 'LOSE' || p.betStatus == 'CANCELLED' ? 'NONE' : parseFloat(p.totalBets) * parseFloat(p.endingOdds)}}</q-item-label> -->
+                                <!-- <q-item-label v-else overline class="text-white">Total Winnings: &nbsp;{{p.betStatus == 'LOSE' || p.betStatus == 'CANCELLED' ? 'NONE' : parseFloat(p.totalBets) * parseFloat(p.endingOdds)}}</q-item-label> -->
+                            </q-item-section>
+
+                            <q-item-section top>
+                            <q-item-label overline class="text-white">{{p.timestamp.toDate('MM-DD-YYYY')}}</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </q-list>
+                    <div class="q-pa-lg flex flex-center">
+                        <q-pagination
+                            v-model="transpage"
+                            :min="transcurrentPage"
+                            :max="Math.ceil(this.transBethistory.length/transtotalPages)"
+                            direction-links
+                            unelevated
+                            color="black"
+                            active-color="yellow"
+                            active-text-color="black"
+                        />
+                    </div>
+                </q-card-section>
+
+                <q-card-actions align="right" class="text-primary">
+                <!-- <q-btn flat label="Cancel" v-close-popup /> -->
+                <q-btn flat label="Ok" v-close-popup />
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
+        <q-dialog v-model="getCredits">
+            <q-card class="bg-secondary text-white">
+                <q-card-section>
+                <div class="text-h6">Claim Credits</div>
+                </q-card-section>
+                <q-separator />
+                <q-card-section style="max-height: 50vh">
+                    <div padding="none" class="row q-gutter-md flex flex-center">
+                        <div padding="none" class="q-pt-md text-center">
+                            <b class="col text-h6">Username: &nbsp;&nbsp;</b>
+                            <b class="text-overline">{{this.username}}</b>
+                        </div>
+                    </div>   
+                    <div padding="none" class="row q-gutter-md">
+                        <div class="q-pt-md">
+                            <b class="col text-h6">Amount to Claim:</b>
+                            <q-input v-model="amount" dark dense type="number" outlined style="width: 200px" />
+                        </div>
+                        <div class="q-pt-md">
+                            <b class="col text-h6">Current Credits:</b>
+                            <q-input v-model="number" disable dark dense type="number" outlined style="width: 200px" />
+                        </div>
+                    </div>
+                </q-card-section>
+                <q-separator />
+
+                <q-card-actions align="right">
+                <q-btn flat label="cancel" color="primary" v-close-popup />
+                <q-btn flat label="Claim" @click="getPlayersCredits()" color="primary" v-close-popup />
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
     </q-page>
 </template>
 <script>
@@ -115,10 +203,22 @@ import { date } from 'quasar'
 export default {
     data(){
         return{
+            username: '',
+            getCredits: false,
+            amount: 0,
+            number: 0,
+            BetHistory: [],
+            BetHistoryOptions: [],
+            transtab: 'bets',
+            myPlayersBetTransactions: false,
             page: 1,
             currentPage:1,
             nextPage: null,
             totalPages:10,
+            transpage: 1,
+            transcurrentPage:1,
+            transnextPage: null,
+            transtotalPages:10,
             TeamGameAccountOptionsBets: [],
             BettorsPlayerID: '',
             tab: 'bet',
@@ -149,6 +249,91 @@ export default {
         }
     },
     methods: {
+        async recordHistory(){
+            await this.myPlayersDetails()
+                let receiveID = this.getID
+                let receiverInfo = this.$lodash.filter(this.Players, m => {
+                        return m['.key'] == receiveID
+                })
+                console.log(receiverInfo, 'receivers')
+                let reciever = {...receiverInfo[0]}
+                let recieverID = reciever['.key']
+                delete reciever['.key']
+                reciever.accountID = recieverID
+
+                let user = this.$store.getters['useraccount/isAuthenticated']
+                console.log(user,'user')
+                let sender = {...user}
+                let senderID = sender.userDBKey
+                delete sender['.key']
+                sender.accountID = senderID
+                console.log(reciever, 'ID')
+                var newHistory = {
+                    amount: this.amount,
+                    admin: sender,
+                    player: reciever,
+                    timestamp: new Date(),
+                }
+                this.$db.collection('ClaimCreditsHistory').add(newHistory)
+        },
+        async getPlayersCredits(){
+            await this.checkWalletBalance()
+            let gettersID = this.getID
+            let credits = this.$lodash.filter(this.Wallet, m => {
+                    return m['.key'] == gettersID
+            })
+            console.log (credits, 'credits..')
+            let currentCredits = credits[0].creditsAmount || 0
+            let currentCommission = credits[0].commisionBalance || 0
+            console.log(currentCommission, 'currentCommission')
+            let total = parseFloat(currentCredits) - parseFloat(this.amount)
+            console.log(credits, 'current')
+            console.log(this.amount, 'amount')
+            var addCreditsBago = {
+                creditsAmount: total,
+                commisionBalance: currentCommission
+            }
+            if(this.amount === '' || this.amount === 0 || this.amount > currentCredits){
+                this.$q.dialog({
+                    title: 'Field Required!',
+                    message: 'Please Enter Right Amount?',
+                    ok: 'Ok',
+                    cancel: 'Cancel'
+                    })
+            }else
+                this.$q.dialog({
+                    title: 'Claim this Credits',
+                    message: 'Get Credits?',
+                    ok: 'Yes',
+                    cancel: 'Cancel'
+                }).onOk(() => { 
+                this.recordHistory()
+                this.$db.collection('Wallet').doc(gettersID).set(addCreditsBago)
+                this.$q.notify({
+                        message: 'Credits Claimed! Refresh the page to see updated Credits',
+                        icon: 'mdi-update',
+                        color: 'positive',
+                        textColor: 'white',
+                        position: 'center'
+                })
+            })
+                    // this.amount = 0
+                    this.getCredits = false
+        },
+        async claimCredits(task) {
+            console.log(task, 'task')
+                this.getID = task['.key']
+                this.amount = 0
+                this.number = task.creditsAmount
+                this.username = task.accountPhone
+                this.getCredits = true
+        },
+        async viewBetTransaction(task){
+            this.TransBettorsPlayerID = task['.key']
+            this.myPlayersBetTransactions = true
+            await this.transPlayersBetHistory()
+            await this.transPlayersBetOptionsHistory()
+        },
         async viewBetHistory(task){
             this.BettorsPlayerID = task['.key']
             this.myPlayersBetHistory = true
@@ -290,6 +475,20 @@ export default {
                 console.error(err)
             })             
         },
+        async transPlayersBetHistory(){
+            await this.$binding("BetHistory", this.$db.collection("BetHistory").where("accountID","==",this.TransBettorsPlayerID))
+            .then((BetHistory) => {
+            }).catch(err => {
+                console.error(err)
+            })             
+        },
+        async transPlayersBetOptionsHistory(){
+            await this.$binding("BetHistoryOptions", this.$db.collection("BetHistoryOptions").where("accountID","==",this.TransBettorsPlayerID))
+            .then((BetHistoryOptions) => {
+            }).catch(err => {
+                console.error(err)
+            })             
+        },
         async playersBetHistory(){
             await this.$binding("TeamGameAccountBets", this.$db.collection("TeamGameAccountBets").where("accountID","==",this.BettorsPlayerID))
             .then((TeamGameAccountBets) => {
@@ -388,6 +587,9 @@ export default {
         },
     },
     computed: {
+        transGetData(){
+			return 	this.transBethistory.slice((this.transpage-1)*this.transtotalPages,(this.transpage-1)*this.transtotalPages+this.transtotalPages)
+        },
         getData(){
 			return 	this.bethistory.slice((this.page-1)*this.totalPages,(this.page-1)*this.totalPages+this.totalPages)
         },
@@ -398,7 +600,15 @@ export default {
             }else {
                 let orderByP = this.$lodash.orderBy(this.TeamGameAccountOptionsBets, ['timestamp'], ['desc']);
                 return orderByP
-                // return this.TeamGameAccountOptionsBets
+            }
+        },
+        transBethistory(){
+            if(this.transtab === 'bets'){  
+                let orderByP = this.$lodash.orderBy(this.BetHistory, ['timestamp'], ['desc']);
+                return orderByP
+            }else {
+                let orderByP = this.$lodash.orderBy(this.BetHistoryOptions, ['timestamp'], ['desc']);
+                return orderByP
             }
         },
         totalBalanceCredit(){
